a4f470932b801ce8250a43d26370e149166de7b4
Gabriel Nunes <74620108+gabrielnuunes@users.noreply.github.com>
2023-09-19 21:53:13 -0300

    Adicionado arquivos base Spring

---
+++ spring-boot-medgest/src/test/java/com/medgest/springbootmedgest/SpringBootMedgestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medgest.springbootmedgest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMedgestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
